4e45b6ec4e961c5522d7cbf0ea9ec12308a593a8
Maxence <coddyumpro@gmail.com>
2024-11-14 13:18:37 +0100

    add result

--- src/searchBar/SearchBar.jsx
+++ src/searchBar/SearchBar.jsx
@@ -1,6 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useKeyCombo from "../Hooks/useKeyCombo";
+import Pizza from "../data/pizza.json";
+
 export default function SearchBar({ setShowInput }) {
+    const [inputValue, setInputValue] = useState("");
+
+    const filterPizza = inputValue
+        ? Pizza.filter((item) => item.id.startsWith(inputValue))
+        : [];
+
     const handleButtonClick = () => {
         setShowInput(false);
     };
@@ -16,18 +24,37 @@ export default function SearchBar({ setShowInput }) {
     useKeyCombo("d", handleButtonClick);
 
     return (
-        <div className="searchBarContainer">
-            <div className="searchBar">
-                <img src="/assets/SVG/searchSVG.svg" alt="search btn" />
-                <input
-                    type="text"
-                    ref={inputRef}
-                    placeholder="Search Pizza..."
-                />
-                <button onClick={handleButtonClick}>
-                    <img src="/assets/SVG/closeSVG.svg" alt="close btn" />
-                </button>
+        <>
+            <div className="searchBarContainer">
+                <div className="searchBar">
+                    <img src="/assets/SVG/searchSVG.svg" alt="search btn" />
+                    <input
+                        type="text"
+                        ref={inputRef}
+                        placeholder="Search Pizza..."
+                        onChange={(i) => setInputValue(i.target.value)}
+                    />
+                    <button onClick={handleButtonClick}>
+                        <img src="/assets/SVG/closeSVG.svg" alt="close btn" />
+                    </button>
+                </div>
             </div>
-        </div>
+
+            {inputValue && (
+                <ul className="resultList">
+                    {filterPizza.length > 0 ? (
+                        filterPizza.map((item, index) => (
+                            <li className="container" key={item.id}>
+                                <h3>{item.name}</h3>
+                                <p>{item.description}</p>
+                                {index < filterPizza.length - 1 && <hr />}
+                            </li>
+                        ))
+                    ) : (
+                        <li>Aucun résultat trouvé</li>
+                    )}
+                </ul>
+            )}
+        </>
     );
 }
